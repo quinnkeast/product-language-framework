c79b86791b31aca60211097153bc0f708ddca231
Quinn Keast <hey@quinnkeast.com>
2023-06-22 12:27:48 +0200

    Include Meta component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Header from './Header';
 import Footer from './Footer';
+import Meta from './Meta';
 
 const Layout = (props) => {
   return (
     <div className='container mx-auto pl-4 pr-4 md:pl-8 md:pr-8'>
+      <Meta />
       <Header />
       {props.children}
       <Footer />
